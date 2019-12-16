33857d7b7858e90413ffc19fd4be89ace5cab501
fo3330 <54835685+fo3330@users.noreply.github.com>
2019-12-16 17:23:32 +0900

    1551028 애플리케이션 프레임워크 수업내용. 조용석.

--- src/main/java/org/dsu/dc/controller/BoardController.java
+++ src/main/java/org/dsu/dc/controller/BoardController.java
@@ -1,11 +1,14 @@
 package org.dsu.dc.controller;
 
 import org.dsu.dc.domain.BoardVO;
+import org.dsu.dc.domain.Criteria;
+import org.dsu.dc.domain.PageDTO;
 import org.dsu.dc.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,9 +24,12 @@ public class BoardController {
 	private BoardService service;
 	
 	@GetMapping("list")
-	public void list(Model model) throws Exception {
-		log.info("[Board] show all list .....");;
-		model.addAttribute("list", service.list());
+	public void list(Criteria cri, Model model) throws Exception {
+		log.info("[Board] show all list .....");
+		model.addAttribute("list", service.getList(cri));
+		int total = service.getTotal(cri);
+		log.info("[Board Total Count : {}", total);
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
 	}
 	
 	@GetMapping("register")
@@ -31,7 +37,7 @@ public class BoardController {
 		
 	}
 	
-	@PostMapping("regiser")
+	@PostMapping("register")
 	public String register(BoardVO board, RedirectAttributes rttr) {
 		log.info("[Board] register: {}", board);
 		service.register(board);
@@ -40,32 +46,43 @@ public class BoardController {
 	}
 	
 	@GetMapping("read")
-	public void read(@RequestParam("bno") Long bno, Model model) {
+	public void read(@RequestParam("bno") Long bno,@ModelAttribute("cri") Criteria cri, Model model) {
 		log.info("[Board] Read... bno={}", bno);
 		model.addAttribute("board", service.get(bno));
+		model.addAttribute("cri",cri);
 	}
 	
 	@GetMapping("modify")
-	public void modify() {
-		
+	public void modify(@RequestParam("bno")Long bno,
+			@ModelAttribute("cri") Criteria cri,Model model) {
+		log.info("[Board] modify.... bno{}", bno);
+		model.addAttribute("board", service.get(bno));
 	}
 	
 	@PostMapping("modify")
-	public String modify(BoardVO board, RedirectAttributes rttr) {
+	public String modify(BoardVO board, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
 		log.info("[Board] modify : {}", board);
 		if(service.modify(board)) {
 			rttr.addFlashAttribute("result", "success");
 		}
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
+		rttr.addAttribute("type", cri.getType());
+		rttr.addAttribute("keyword", cri.getKeyword());
 		return "redirect:/board/list";
 	}
 	
-	@GetMapping("remove")
-	public String remove(@RequestParam("bno") Long bno,
+	@PostMapping("remove")
+	public String remove(@RequestParam("bno") Long bno,@ModelAttribute("cri") Criteria cri,
 			RedirectAttributes rttr) {
 		log.info("{board] Remove... bno={}", bno);
 		if(service.remove(bno)) {
 			rttr.addFlashAttribute("result", "success");
 		}
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
+		rttr.addAttribute("type", cri.getType());
+		rttr.addAttribute("keyword", cri.getKeyword());
 		return "redirect:/board/list";
 	}
 }
